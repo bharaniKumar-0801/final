fefc29b3220f1bc347be5b29e245756ceb5d121d
bharaniKumar-0801 <bharani@intuitiveobjects.com>
2025-08-25 11:16:37 +0530

    Delete intuitiveobjects-rag-ui directory

--- intuitiveobjects-rag-ui/src/components/atoms/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode;
-  className?: string;
-}
-
-export const Container = ({ children, className = "" }: ContainerProps) => {
-  return <div className={`max-w-3xl mx-auto ${className}`}>{children}</div>;
-};
--- intuitiveobjects-rag-ui/src/utils/function.ts
+++
@@ -1,5 +0,0 @@
-export const formatFileSize = (sizeInBytes: number) => {
-    if (sizeInBytes < 1024) return `${sizeInBytes} B`;
-    if (sizeInBytes < 1024 * 1024) return `${(sizeInBytes / 1024).toFixed(1)} KB`;
-    return `${(sizeInBytes / (1024 * 1024)).toFixed(1)} MB`;
-};
